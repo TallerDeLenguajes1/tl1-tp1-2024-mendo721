50bafa29b809b732650ec828833cbea3767d0732
Rodrigo <rodrigomendoza721@gmail.com>
2024-04-14 22:54:45 -0300

    segundo commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,5 +3,14 @@
 int main(int argc, char const *argv[])
 {
     printf("Hola mundo");
+    int *punt, num = 9;
+    punt = &num;
+    printf("\nEl contenido del puntero: %d", *punt);
+    printf("\nLa direccion que almacena el puntero: %p", punt);
+    printf("\nLa direccion de la variable num: %p", &num);
+    printf("\nLa direccion de memoria del puntero es: %p", &punt);
+    printf("El tamanio de memoria utilizado por la variable es: %zu bytes\n", sizeof(num));
+
+
     return 0;
 }
